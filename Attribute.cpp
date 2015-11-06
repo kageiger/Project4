efec25ce0139977746b39a6f67a36f3274b2bcaa
keyaria <keyariaw@umich.edu>
2015-11-06 16:03:35 -0500

    Attribute.cpp

---
+++ Attribute.cpp
@@ -0,0 +1,66 @@
+/**
+ * Attribute.cpp
+ *
+ * <#Names#>
+ * <#Uniqnames#>
+ *
+ * EECS 183: Project 4
+ *
+ * <#description#>
+ */
+
+#include "Attribute.h"
+
+
+Attribute::Attribute() {
+    // TODO: implement
+}
+
+
+int Attribute::getValueWithinRange(int value) {
+    // TODO: implement
+
+    // DELETE THIS: returning an int to prevent compile error
+    return 0;
+}
+
+
+Attribute::Attribute(int inIndex, int inPoints) {
+    // TODO: implement
+}
+
+
+int Attribute::getIndex()  {
+    // TODO: implement
+
+    // DELETE THIS: returning an int to prevent compile error
+    return 0;
+}
+
+
+int Attribute::getPoints()  {
+    // TODO: implement
+
+    // DELETE THIS: returning an int to prevent compile error
+    return 0;
+}
+
+
+void Attribute::setIndex(int inIndex) {
+    // TODO: implement
+}
+
+
+void Attribute::setPoints(int inPoints) {
+    // TODO: implement
+}
+
+
+void Attribute::read(istream &ins) {
+    // TODO: implement
+}
+
+
+void Attribute::write(ostream &outs)  {
+    // TODO: implement
+}
