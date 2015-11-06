ebb745bbb09a456cf2999db3b449dfa63a118d1c
keyaria <keyariaw@umich.edu>
2015-11-06 16:01:07 -0500

    Answer.cpp

---
+++ answer.cpp
@@ -0,0 +1,27 @@
+/**
+ * Answer.cpp
+ *
+ * <#Names#>
+ * <#Uniqnames#>
+ *
+ * EECS 183: Project 4
+ *
+ * <#description#>
+ */
+
+#include "Answer.h"
+
+Answer::Answer() {
+}
+
+// TODO: getAttribute, setAttribute, getText, setText
+
+
+void Answer::read(istream &ins) {
+    // TODO: implement
+}
+
+
+void Answer::write(ostream &outs) {
+    // TODO: implement
+}
